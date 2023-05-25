5656942228408a5f996f2c2cf08460a2590567d3
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-05-25 09:46:26 +0200

    document work-around for password root login

--- sshd.ps1
+++ sshd.ps1
@@ -3,3 +3,8 @@ Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Get-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+
+echo "> allow root login? ... add the following configs:"
+echo "PermitRootLogin no"
+echo "MaxAuthTries 20"
+start-process notepad C:\Programdata\ssh\sshd_config
